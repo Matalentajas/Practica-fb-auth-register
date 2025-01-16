9bd8ceaaa5fdd4ca3f8811f720521868637172fa
Arturo Orient Romero <arturorient1305@gmail.com>
2025-01-16 09:49:34 +0100

    Enlazamos el proyecto con FireBase

---
+++ src/db/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHi-A7fe3D6dQIzEhPYSd1mVeEFWcgqj0",
+  authDomain: "fir-auth-5b627.firebaseapp.com",
+  projectId: "fir-auth-5b627",
+  storageBucket: "fir-auth-5b627.firebasestorage.app",
+  messagingSenderId: "187248487370",
+  appId: "1:187248487370:web:fa317779136bd7eb0bbd33"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); // Conexion a la autenticacion de firebase
+export { auth };
